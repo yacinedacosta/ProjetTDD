c8a1ee1efb431422165337f5bd83e345248718f1
yacinedacosta <yacine.dacosta@protonmail.com>
2024-03-23 10:16:34 +0100

    Modifications du 22/03

--- typing.ml
+++ typing.ml
@@ -5,8 +5,13 @@ type environment =
     {localvar: (vname * tp) list; 
      funbind: (vname * fpdecl) list}
 
+let rec buildType listeArgs = match listeArgs with
+| [Vardecl(_, tp)] -> tp
+| (Vardecl(_, tp) :: suite) -> FunT(tp, buildType(suite))
+| [] -> failwith "Je comprends pas"
+
 let tp_fun fpdecl = match fpdecl with
-| FPdecl(tp, _, _) -> tp 
+| FPdecl(tp, _, listeArguments) -> FunT(tp, buildType(listeArguments))
 
 (* tp_var_local : (vname * tp) list -> vname -> tp option      avec tp option = Some tp ou None*)
 let rec tp_var_local envLocal var = match envLocal with
@@ -32,7 +37,13 @@ let rec tp_var env var = match tp_var_local env.localvar var with
 |Some t -> t
 
 (*TO DO*)
-let tp_application fct liste_args = IntT
+let rec tp_application fct liste_args = match liste_args with
+| [] -> fct
+| (tp :: reste) -> match fct with 
+        | FunT(type1, type2) -> if type1 = tp 
+                                then tp_application type2 reste
+                                else failwith "Les arguments ne correspondent pas a la définition de la fonction !"
+| _ -> failwith "Err"
 
 (* tp_binop : operateur -> tp -> tp -> tp *)
 let tp_binop operateur toperande1 toperande2 = match operateur with
@@ -72,6 +83,9 @@ let rec tp_expr env exp = match exp with
         then tp_expr env e2 
         else failwith "Types différents dans les deux branches !"
 |_-> failwith "Mais qu'est-ce que c'est ???"
+
+
+let tp_fdefn env fdefn = IntT
 
 (* TODO: implement *)
 let tp_prog (Prog (fdfs, e)) = IntT
