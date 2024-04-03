2afd6ce67cbd251dbac7061970f84d3cd6737691
yacinedacosta <yacine.dacosta@protonmail.com>
2024-04-03 20:11:54 +0200

    Tests eratosthene et fibonacci

--- Tests/CribleEratosthene.ml
+++ Tests/CribleEratosthene.ml
@@ -1,37 +1,42 @@
-
-let rec generer = fun 
+(* Test pour la récursivité terminale et pour les appels de fonction *)
+let rec generer = function
 | 2 -> [2]
 | n -> generer(n-1)@[n] ;;
-(*generer : int -> int list *) 
 
+(*
+generer : int -> int list  
 generer 6 ;;
-(*- : int list = [2; 3; 4; 5; 6]*)
+- : int list = [2; 3; 4; 5; 6]
+*)
 
-let rec eliminer = fun 
+let rec eliminer = function
 | (a,x::q) -> if x mod a = 0 
 				then eliminer (a,q)
 				else x :: eliminer(a,q)
 | (_ , []) -> [] ;;
-(*eliminer : int * int list -> int list*)
 
+(*
+eliminer : int * int list -> int list
 eliminer (2 , [1;2;3;4;5;6] );;
-(*- : int list = [1; 3; 5] *)
+- : int list = [1; 3; 5] 
+*)
 
-let rec eratosAux = fun 
+let rec eratosAux = function
 | (a :: q) -> a :: eratosAux(eliminer(a,q)) 
 | [] -> [] ;;
 
 let eratos = fun n ->
 	let liste = generer(n) in 
 	eratosAux liste ;;
-(*eratos : int -> int list *)
-
+(*eratos : int -> int list
 eratos 10 ;;
-(*- : int list = [2; 3; 5; 7] *)
+- : int list = [2; 3; 5; 7] *)
+
 eratos 20 ;;
 (*- : int list = [2; 3; 5; 7; 11; 13; 17; 19]) *)
 
-let rec jumeaux = fun 
+(*
+let rec jumeaux = function
 | (a::b::q) -> if b-a = 2 then (a,b):: jumeaux(b::q) 
 				else jumeaux (b::q)
 | _ -> [] ;;
@@ -45,5 +50,6 @@ let listeJumeaux = fun n -> jumeaux (eratos n) ;;
 
 listeJumeaux 20 ;;
 (*- : (int * int) list = [3, 5; 5, 7; 11, 13; 17, 19] *)
+*)
 
 
--- Tests/fibonacci.ml
+++ Tests/fibonacci.ml
@@ -1,9 +1,9 @@
-let Fibonacci (n : int) =
-	let rec fiboAux = fun 
+let fibonacci (n : int) =
+	let rec fiboAux = function
 	| (_,_,0) -> []
 	| (a,b,n) -> a::fiboAux(b,a+b,n-1)
 	in fiboAux (1,1,n) ;; 
 (*Fibonacci : int -> int list = <fun>*)
 
-(*Fibonacci 8 ;;*)
+fibonacci 8 ;;
 (*- : int list = [1; 1; 2; 3; 5; 8; 13; 21]*)
